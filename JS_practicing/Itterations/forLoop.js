762f6ed9692dd527f2ec67568fafb6e5d1b35f33
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2023-10-29 11:42:23 +0500

    JS loops clear

---
+++ JS_practicing/Itterations/forLoop.js
@@ -0,0 +1,64 @@
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     console.log(element);
+
+// }
+
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element === 5) {
+//         console.log("element is 5");
+//     }
+
+//     console.log(element);
+
+// }
+
+// (() => {
+//     for (let i = 1; i <= 10; i++) {
+//         console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
+//         for (let j = 1; j <= 10; j++) {
+//             console.log(`${i} * ${j} = ${i * j}`);
+//         }
+//     }
+// })()
+
+
+// const superHeros = ['allama Iqbal', 'quaid e azam', 'abdul qadeer khan', 'abdul hameed', 'abdul hameeeed', 'abdul hameeed']; 
+// // console.log(superHeros.length);
+// for (let i = 0; i < superHeros.length; i++) {
+//     const element = superHeros[i];
+//     console.log(element);
+// }
+
+
+
+const superHeros = ['allama Iqbal', 'quaid e azam', 'abdul qadeer khan', 'abdul hameed', 'abdul hameeeed', 'abdul hameeed'];
+
+// const arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+for (let i = 0; i < superHeros.length; i++) {
+    if (i == 6) {
+        const element = superHeros.length[i];
+        // console.log(`superHeros number is ${Object.values}`);
+        console.log(element);
+        break;
+    }
+    console.log(superHeros[i]);
+}
+console.log(superHeros[i]);
+
+// const arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+// for (let i = 0; i < arr.length; i++) {
+//     if (i == 6) {
+//         // console.log(`arr number is ${Object.values}`);
+//         continue;   
+//     }
+//     console.log(i);
+// }
+
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }

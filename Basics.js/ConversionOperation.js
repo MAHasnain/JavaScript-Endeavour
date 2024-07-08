8987e9b5ebd72be13631c0c8021c2059a9a85385
Muhammad_Atta_Ul_Hasnain <attaulhasnainmaqsoodi@gmail.com>
2024-07-09 04:21:55 +0500

    Revision study

---
+++ Basics.js/ConversionOperation.js
@@ -0,0 +1,19 @@
+// CONVERSION OPERATION
+
+"use strict";
+
+let name = "hasnain";
+let age = "22n";
+let married = false;
+
+console.log(typeof age);
+
+let ageConverter = Number(age)
+let booleanConverter = Number(married)
+
+console.log(ageConverter);
+console.log(booleanConverter);
+console.log(typeof ageConverter);
+console.log(typeof booleanConverter);
+
+
--- Basics.js/Datatypes.js
+++ Basics.js/Datatypes.js
@@ -1,55 +1,66 @@
+"use strict";
+// var v = 4;
+// var c = 6; 
+// var b = c + v; 
+// console.log(b);
 
-var v = 4;
-var c = 6; 
-var b = c + v; 
-console.log(b);
-
-var hieght = 15; 
-var weight = 98;
-var square = weight - hieght; 
-console.log(square);
-
-
-var myName = "hasnain";
-var shirtColor = "white";
-
-console.log(shirtColor);
-console.log(myName); //statement one 
+// var hieght = 15; 
+// var weight = 98;
+// var square = weight - hieght; 
+// console.log(square);
 
 
-var myBike = {
-    name: "harleyDavidson",
-    modelNumber: "Has432",
-    number: 12345,    
-};
+// var myName = "hasnain";
+// var shirtColor = "white";
 
-console.log(myBike.name);
-console.log(myBike.modelNumber);
-console.log(myBike.number);
-
-console.log(myBike);
+// console.log(shirtColor);
+// console.log(myName); //statement one 
 
 
-var the = 1;
-var first = 4;
-var name = 3;
+// var myBike = {
+//     name: "harleyDavidson",
+//     modelNumber: "Has432",
+//     number: 12345,    
+// };
 
-var lastName = the - first - name;
-console.log(lastName);
+// console.log(myBike.name);
+// console.log(myBike.modelNumber);
+// console.log(myBike.number);
 
-
-var bike = "harley davidson";
-var car = "audi_A4";
-
-console.log(car);
-console.log(bike);
+// console.log(myBike);
 
 
-var atta = {
- myName: "hasnain",
- marks: 75,
-}
+// var the = 1;
+// var first = 4;
+// var name = 3;
 
-console.log(atta.myName);
-console.log(atta.marks);
+// var lastName = the - first - name;
+// console.log(lastName);
 
+
+// var bike = "harley davidson";
+// var car = "audi_A4";
+
+// console.log(car);
+// console.log(bike);
+
+
+// var atta = {
+//  myName: "hasnain",
+//  marks: 75,
+// }
+
+// console.log(atta.myName);
+// console.log(atta.marks);
+
+
+
+// Number   427
+// null   (empty) / standalone value
+// String   "MAHs"
+// Symbol    unique
+// BigInt       
+// Boolean     true/false
+// undefined    (undefined)
+
+console.log(typeof );

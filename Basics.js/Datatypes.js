801bb358e27218bb62738d4f56c2e25e9c6d498b
Muhammad_Atta_Ul_Hasnain <attaulhasnainmaqsoodi@gmail.com>
2024-07-09 22:45:08 +0500

    memory concept revised

--- Basics.js/Datatypes.js
+++ Basics.js/Datatypes.js
@@ -69,6 +69,66 @@
 // undefined    (undefined)
 
 console.log(typeof null);
+console.log(typeof undefined);
+
+let id = Symbol(12345);
+let otherId = Symbol(12345);
+
+console.log(id);
+console.log(typeof otherId);
+// console.log(BigInt);
+
 
 
 ////  NON PRIMITIVE DATA TYPES   (return object)
+
+const myfunc = function mah(age, hieght, weight) {
+    age = 23;
+    hieght = 56;
+    weight = 59
+}
+
+console.log(typeof myfunc);
+// return (age, hieght, weight);
+
+let obj = [1, 3, 5, 7, 9];
+console.log(typeof obj);
+
+
+
+// MEMORY ALLOCATION
+// PRIMITIVE                                NON-PRIMITIVE
+//  STACK                                       HEAP
+// (data ka copy milta h)                     (original value ka referance milta h yani original value me hi changes hongy)
+
+let myName = "MAH";
+let shortForm = myName;
+
+shortForm = "Atta";
+
+console.log(shortForm);
+console.log(myName);
+
+
+let lang = "javascript";
+let myFavouriteLang = lang;
+
+console.log(myFavouriteLang);
+myFavouriteLang = "JS";
+
+console.log(myFavouriteLang);
+console.log(lang);
+
+let myObj = {
+    name : "mah",
+    age : 22,
+    email : "mah@gmailcom"
+}
+
+let myObj2 = myObj;
+
+myObj2.email = "mah@emailcom";
+
+console.log(myObj2.email);
+console.log(myObj.email);
+

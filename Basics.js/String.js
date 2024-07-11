eed18dea5692338788175b36ef00fe48f5960011
Muhammad Atta Ul Hasnain <134517609+MAHasnain@users.noreply.github.com>
2024-07-11 08:49:35 +0000

    20 string methods add

--- Basics.js/String.js
+++ Basics.js/String.js
@@ -1,5 +1,28 @@
 // String  and methods 
 
+// String length
+// String charAt()
+// String charCodeAt()
+// String at()
+// String [ ]
+// String slice()
+// String substring()
+// String substr()
+// String toUpperCase()
+// String toLowerCase()
+// String concat()
+// String trim()
+// String trimStart()
+// String trimEnd()
+// String padStart()
+// String padEnd()
+// String repeat()
+// String replace()
+// String replaceAll()
+// String split()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+
+
+
 let myName = "MAHasnain";
 let age = 23;
 

0b0c5c0b695750abfa5f7e0f402a5505ccd73880
Muhammad Atta Ul Hasnain <134517609+MAHasnain@users.noreply.github.com>
2024-07-12 04:52:49 +0000

    number and its methods learning

---
+++ Basics.js/numbers_and_maths.js
@@ -0,0 +1,24 @@
+const score = 4333;
+
+const balance = new Number (100);
+
+console.log(balance);
+
+
+
+const amount = 10000000;
+console.log(amount.toLocaleString('en-PK'));
+
+
+const amount2 = 145.4546;
+console.log(amount2.toPrecision(5));
+
+const otherNumber = 346.383;
+console.log(otherNumber.toFixed());
+
+
+
+
+
+console.log(Math)
+

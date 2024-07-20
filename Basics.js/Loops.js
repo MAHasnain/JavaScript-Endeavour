a84f6837d61d75ea201418873c98cd4bcf15e362
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2024-07-20 17:36:49 +0500

    loops learned almost

---
+++ Basics.js/Loops.js
@@ -0,0 +1,44 @@
+// While loop 
+
+
+// const myArray = ["hasnain", "ahsan", "husain", "hassan"];
+// let arr = 0;
+
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`)
+//     arr = arr + 1;
+// };
+
+// let nArr = ["hasnain", "husain", "hassan", "ahsan"];
+
+// let variableForIncrement = 0;
+
+// while (variableForIncrement < nArr.length) {
+//     console.log(`Value is ${nArr[variableForIncrement]}`)
+//     variableForIncrement = variableForIncrement + 1;
+// };
+
+
+
+
+
+// const newArr = ["mah", "atta", "hasnain"];
+// let arr = 0;
+
+// while (arr < newArr.length) {
+//     console.log(`value is ${newArr[arr]}`)
+//     arr = arr + 1 ;    
+// }
+
+
+
+// pehly kaam kryga then condition check kreyga
+
+let score = 0; 
+    
+do {            
+    console.log(`score is ${score}`)
+    score ++ ;
+} while (score < 10);
+
+
